e6ed64cd6b39102b67ed45fdc399db87f7905087
Nhi Phan <b.nhi291@gmail.com>
2023-08-30 21:19:58 +1000

    70% complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
-    
+
+    // Makes the private variable visible in the Inspector
+    [SerializeField]
+    private  GameObject blueObj;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Limit the frame rate to 60
+        Application.targetFrameRate = 60;
         
+        // Instantiate redObj at (2, 0, 0) with zero rotation
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+
+        // Instantiate blueObj at (-2, 0, 0) with zero rotation
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
